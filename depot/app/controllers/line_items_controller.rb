a9544f5a639d4c6c83df682cadc19bd93fe0e5b3
padmasree <padma@agiratech.com>
2016-09-01 19:14:09 +0530

    devoloping application for moving the cart to sidebar and creating cart using ajax

--- depot/app/controllers/line_items_controller.rb
+++ depot/app/controllers/line_items_controller.rb
@@ -25,18 +25,19 @@ class LineItemsController < ApplicationController
 
   # POST /line_items
   # POST /line_items.json
-  def create
+def create
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product.id)
     respond_to do |format|
     if @line_item.save
-    format.html { redirect_to @line_item.cart }
+    format.html { redirect_to store_index_url }
+    format.js   { @current_item = @line_item }
     format.json { render action: 'show',
     status: :created, location: @line_item }
     else
     format.html { render action: 'new' }
     format.json { render json: @line_item.errors,
-    status: :unprocessable_entity }
+      status: :unprocessable_entity }
     end
     end
 end
