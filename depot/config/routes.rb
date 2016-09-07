4e9c09acbe66ab81eaa626ea3ed597e3c155d3b2
padmasree <padma@agiratech.com>
2016-09-07 14:18:08 +0530

    devoloping application logging in and internationalization

--- depot/config/routes.rb
+++ depot/config/routes.rb
@@ -1,17 +1,32 @@
 Depot::Application.routes.draw do
-  resources :orders
-  resources :line_items
-  resources :carts
-  get 'store/index'
+    get 'admin' => 'admin#index'
+    controller :sessions do
+      get 'login' => :new
+      post 'login' => :create
+      delete 'logout' => :destroy
+end
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  resources :users
+
 
   resources :products do
     get :who_bought, on: :member
 end
+   scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    get 'store/index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'store#index'
+end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
